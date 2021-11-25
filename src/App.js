68b9cfc2798c76e111a812ca8eb9f9816b7d74fa
Lishu Gupta <lishugupta652@gmail.com>
2021-11-25 10:53:20 +0000

    axios

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import "./App.css";
 import axios from "axios";
-
+import { useEffect, useState } from "react";
+import { Container } from "@material-ui/core";
 function App() {
+  const [word, setWord] = useState("");
+  const [meanings, setMeanings] = useState([]);
   const getDictionary = async () => {
     try {
       const data = await axios.get(
         "https://api.dictionaryapi.dev/api/v2/entries/en/test"
       );
+      console.log(data);
+      setMeanings(data.data);
     } catch (error) {
       console.log(error);
     }
   };
 
-  return <div className="App"></div>;
+  useEffect(() => {
+    getDictionary();
+  }, []);
+  return (
+    <div className="App">
+      <Container></Container>
+    </div>
+  );
 }
 
 export default App;
